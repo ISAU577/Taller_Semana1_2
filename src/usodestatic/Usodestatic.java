a53ec3c10ca71d220edb67b515efb3986c335233
IsauJorge <condeisau@gmail.com>
2026-05-31 21:01:26 -0500

    Caso 4 completado: Uso de variables y metodos static

--- src/usodestatic/Usodestatic.java
+++ src/usodestatic/Usodestatic.java
@@ -1,5 +1,31 @@
 package usodestatic;
 
 public class Usodestatic {
+    
+  
+    private static int contador = 0;
 
+  
+    public Usodestatic() {
+        contador++;
+    }
+
+    
+    public static int getContador() {
+        return contador;
+    }
+
+  
+    public static void main(String[] args) {
+     
+        System.out.println("Objetos creados al inicio: " + Usodestatic.getContador());
+
+      
+        new Usodestatic();
+        new Usodestatic();
+        new Usodestatic();
+
+        System.out.println("Objetos creados al final: " + Usodestatic.getContador());
+    }
 }
+
