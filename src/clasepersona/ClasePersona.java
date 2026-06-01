035bdbdb873fb0672b1f96f9a0c5e7283f022192
IsauJorge <condeisau@gmail.com>
2026-05-31 20:39:14 -0500

    Caso 2 completado: Implementacion de la clase Persona con POO

--- src/clasepersona/ClasePersona.java
+++ src/clasepersona/ClasePersona.java
@@ -2,4 +2,44 @@ package clasepersona;
 
 public class ClasePersona {
 
+    private String nombre;
+    private int edad;
+
+    public ClasePersona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad >= 0) { 
+            this.edad = edad;
+        }
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + nombre + " | Edad: " + edad + " años");
+    }
+
+    
+    public static void main(String[] args) {
+        ClasePersona persona1 = new ClasePersona("Jorge Gómez", 22);
+        ClasePersona persona2 = new ClasePersona("Robert Ruiz", 28);
+
+        System.out.println("--- Información de las Personas ---");
+        persona1.mostrarDatos();
+        persona2.mostrarDatos();
+    }
 }
+
